805e65610b6085221ea59212d46ee2ee225c2f55
devshri <devshri123@gmail.com>
2024-01-29 22:12:19 +0530

    added basic infrastructure of framework

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+public class LoginPage {
+	
+	public void loginToApp()
+	{
+		System.out.println("Login To Application");
+	}
+    
+	
+	
+	public void  fetchTittle()
+	{
+		System.out.println("Getting the title");
+	}
+
+}
